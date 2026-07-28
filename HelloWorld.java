fd7424add10c7830bfe43af75bb009e3964f2546
Anjan  Kumar Guha <66314225+Anjan26@users.noreply.github.com>
2026-07-29 02:57:30 +0530

    Update console message for Jenkins integration

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // This line prints the message to the console
-        System.out.println("the jenkins integration is successful, code is compiled and executed");
+        System.out.println("the jenkins integration is successful, code is compiled and executed and build automatically!");
     }
 }
